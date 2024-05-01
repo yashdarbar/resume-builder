fcae6f705fb1f47b90bc4a378852dd6d73e950f5
Yash Darbar <darbaryash44@gmail.com>
2024-05-02 04:11:01 +0530

    almost done with the UI

--- app/page.js
+++ app/page.js
@@ -178,20 +178,20 @@ export default function Home(props) {
         //     </a>
         //   </div>
         // </main>
-        <div className="flex text-black bg-white font-semibold w-full">
-            <div className="w-1/2 flex flex-col mt-5 min-h-screen max-h-fit mb-8       ">
-                <div className="mx-auto">
+        <div className="flex text-black bg-violet-400 font-semibold w-full">
+            <div className="w-1/2 flex flex-col mt-8 min-h-screen max-h-fit mb-8       ">
+                <div className="mx-auto flex flex-col">
                     <h1 className="text-center text-2xl mb-2">Add details</h1>
-                    <div className="w-96">
-                        <h2 className="text-sm">Name</h2>
+                    <div className="w-96 m-2 mt-4 bg-white rounded-md px-4 py-1">
+                        <h2 className="text-sm mt-2">Name</h2>
                         <input
                             value={text}
                             message={text}
                             onChange={textHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         ></input>
                         <h2 className="text-sm">Contact</h2>
-                        <div className="flex  w-11/12 justify-between">
+                        <div className="flex  w-full justify-between">
                             <input
                                 value={contact}
                                 //value={contact}
@@ -213,7 +213,7 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={projectHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         />
                         <h2 className="text-sm">Education</h2>
                         <input
@@ -221,7 +221,7 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={educationHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         ></input>
                         <h2 className="text-sm">Languages</h2>
                         <input
@@ -229,7 +229,7 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={languagesHandler}
-                            className="mb-2 w-11/12 p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full p-1 bg-gray-200 rounded-md"
                         ></input>
                         <h2 className="text-sm">Frameworks</h2>
                         <input
@@ -237,7 +237,7 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={frameworksHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         ></input>
                         <h2 className="text-sm">Tools</h2>
                         <input
@@ -245,7 +245,7 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={toolsHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         ></input>
                         <h2 className="text-sm">Experience</h2>
                         <textarea
@@ -253,7 +253,7 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={experienceHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         />
                         <h2 className="text-sm">Links</h2>
                         <input
@@ -261,16 +261,16 @@ export default function Home(props) {
                             //value={contact}
                             //message={text}
                             onChange={linksHandler}
-                            className="mb-2 w-11/12  p-1 bg-gray-200 rounded-md"
+                            className="mb-2 w-full  p-1 bg-gray-200 rounded-md"
                         ></input>
                     </div>
-                    <Button
-                        className="w-fit mt-3"
+                    {/* <Button
+                        className=" mx-auto text-center"
                         onClick={handler}
                         primary={true}
                     >
                         Primary Button
-                    </Button>
+                    </Button> */}
                 </div>
                 {/* <Button
                     className="w-fit mt-6"
@@ -280,41 +280,60 @@ export default function Home(props) {
                     Primary Button
                 </Button> */}
             </div>
-            <div className="flex flex-col text-left text-black  mt-5 min-h-screen max-h-fit w-1/2">
-                <h1 className="text-center text-2xl mb-2">Resume</h1>
-                <PDFExport ref={pdfconverter} paperSize="A4" class="container">
-                    <div className="mx-auto my-5 w-4/5 bg-blue-300 text-xs p-4 rounded-md text-justify">
+            <div className="flex flex-col text-left text-black  mt-8 min-h-screen max-h-fit w-1/2">
+                <h1 className="text-center text-2xl mb-4">Resume</h1>
+                <PDFExport ref={pdfconverter} paperSize="A4" >
+                    <div className="mx-auto my-2 w-4/5 bg-white text-xs p-4 rounded-md text-justify">
                         <span className="">
                             <h2 className="text-2xl">{text}</h2>
-                            <h2 className="text-base mt-2">Contact:</h2>
+                            <h2 className="text-base mt-2 font-medium">
+                                Contact:
+                            </h2>
                             <hr className="h-1" />
                             <h2>{contact}</h2>
                             <h2>{contactTwo}</h2>
                         </span>
-                        <h2 className="text-base mt-2">Projects: </h2>
+                        <h2 className="text-base mt-2 font-medium">
+                            Projects:{" "}
+                        </h2>
                         <hr className="h-1" />
                         <p className="">{project}</p>
-                        <h2 className="text-base mt-2">Education:</h2>
+                        <h2 className="text-base mt-2 font-medium">
+                            Education:
+                        </h2>
                         <hr className="h-1" />
                         <span className="w-full"> {education}</span>
-                        <h2 className="text-base mt-2">Languages: </h2>
+                        <h2 className="text-base mt-2 font-medium">
+                            Languages:{" "}
+                        </h2>
                         <hr className="h-1" />
                         <span>{languages}</span>
-                        <h2 className="text-base mt-2">Frameworks: </h2>
+                        <h2 className="text-base mt-2 font-medium">
+                            Frameworks:{" "}
+                        </h2>
                         <hr className="h-1" />
                         <span>{frameworks}</span>
-                        <h2 className="text-base mt-2">Tools: </h2>
+                        <h2 className="text-base mt-2 font-medium">Tools: </h2>
                         <hr className="h-1" />
                         <span>{tools}</span>
-                        <h2 className="text-base mt-2">Experience: </h2>
+                        <h2 className="text-base mt-2 font-medium">
+                            Experience:{" "}
+                        </h2>
                         <hr className="h-1" />
                         <p>{experience}</p>
-                        <h2 className="text-base mt-2">Links: </h2>
+                        <h2 className="text-base mt-2 font-medium">Links: </h2>
                         <hr className="h-1" />
                         <span>{links}</span>
-                        <p>lorem lafaljf ksdj i wnat to go to himalays</p>
+                        <p></p>
                     </div>
                 </PDFExport>
+                <Button
+                    className=" mx-auto mt-1 text-center"
+                    onClick={handler}
+                    primary={true}
+                >
+                    Download
+                </Button>
             </div>
 
             {/* <Resume data={data}/> */}
